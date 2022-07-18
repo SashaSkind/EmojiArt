7ba6a620524e112c63a8a8119414ed36ace23078
Sasha <a.skinderev@gmail.com>
2022-07-18 18:07:49 +0300

    App has documents now, and file system

--- EmojiArt/EmojiArtApp.swift
+++ EmojiArt/EmojiArtApp.swift
@@ -9,14 +9,11 @@ import SwiftUI
 
 @main
 struct EmojiArtApp: App {
-    // L12 mark these as @StateObject since they are "sources of truth"
-    @StateObject var document = EmojiArtDocument()
     @StateObject var paletteStore = PaletteStore(named: "Default")
     
     var body: some Scene {
-        WindowGroup {
-            EmojiArtDocumentView(document: document)
-                // L12 "inject" our PaletteStore ViewModel into our View hierarchy
+        DocumentGroup(newDocument: { EmojiArtDocument() }) { config in
+            EmojiArtDocumentView(document: config.document)
                 .environmentObject(paletteStore)
         }
     }
