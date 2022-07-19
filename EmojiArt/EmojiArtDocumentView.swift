a6b764febb65614976a046c99d681bcf36c13f26
Sasha <a.skinderev@gmail.com>
2022-07-19 03:01:46 +0300

    Undo, redo and paste bottoms in a good placement now

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -64,12 +64,29 @@ struct EmojiArtDocumentView: View {
                 }
             }
             .toolbar {
-                UndoButton(
-                    undo: undoManager?.optionalUndoMenuItemTitle,
-                    redo: undoManager?.optionalRedoMenuItemTitle
-                )
+                ToolbarItem(placement: .bottomBar) {
+                    AnimatedActionButton(title: "Paste Background", systemImage: "doc.on.clipboard") {
+                        pasteBackground()
+                    }
+                    if let undoManager = undoManager {
+                        if undoManager.canUndo {
+                            AnimatedActionButton(title: undoManager.undoActionName, systemImage: "arrow.uturn.backward") {
+                                undoManager.undo()
+                            }
+                        }
+                        if undoManager.canRedo {
+                            AnimatedActionButton(title: undoManager.redoActionName, systemImage: "arrow.uturn.forward") {
+                                undoManager.redo()
+                            }
+                        }
+                    }
+                }
             }
         }
+    }
+    
+    private func pasteBackground() {
+        
     }
     
     @State private var autozoom = false
